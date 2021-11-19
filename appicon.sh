3494da14a5235f9f1edef43c1da8137b64b5492d
ckosmic <christiankosman@gmail.com>
2021-11-19 00:55:43 -0800

    Fix icons for Apple TV 4K

--- appicon.sh
+++ appicon.sh
@@ -17,9 +17,12 @@ if brew ls --versions imagemagick > /dev/null; then
     
     
     #tvOS App Icons
-    convert -size 400X240 gradient:'#0390fc'-'#00589c' "sm64ios/sm64tvos/Assets.xcassets/AppIcon.brandassets/App Icon.imagestack/Back.imagestacklayer/Content.imageset/back.png"
+    convert -size 400X240 gradient:'#0390fc'-'#00589c' "sm64ios/sm64tvos/Assets.xcassets/AppIcon.brandassets/App Icon.imagestack/Back.imagestacklayer/Content.imageset/back400x240.png"
+        convert -size 800X480 gradient:'#0390fc'-'#00589c' "sm64ios/sm64tvos/Assets.xcassets/AppIcon.brandassets/App Icon.imagestack/Back.imagestacklayer/Content.imageset/back800x480.png"
     convert -interpolate Nearest -filter point -resize 180X180 "textures/segment2/segment2.05A00.rgba16.png" "sm64ios/sm64tvos/Assets.xcassets/AppIcon.brandassets/App Icon.imagestack/Front.imagestacklayer/Content.imageset/head.png"
-    convert -interpolate Nearest -filter point -gravity center -extent 400X240 -background none -transparent white  "sm64ios/sm64tvos/Assets.xcassets/AppIcon.brandassets/App Icon.imagestack/Front.imagestacklayer/Content.imageset/head.png" "sm64ios/sm64tvos/Assets.xcassets/AppIcon.brandassets/App Icon.imagestack/Front.imagestacklayer/Content.imageset/front.png"
+    convert -interpolate Nearest -filter point -gravity center -extent 400X240 -background none -transparent white  "sm64ios/sm64tvos/Assets.xcassets/AppIcon.brandassets/App Icon.imagestack/Front.imagestacklayer/Content.imageset/head.png" "sm64ios/sm64tvos/Assets.xcassets/AppIcon.brandassets/App Icon.imagestack/Front.imagestacklayer/Content.imageset/front400x240.png"
+    convert -interpolate Nearest -filter point -resize 360X360 "textures/segment2/segment2.05A00.rgba16.png" "sm64ios/sm64tvos/Assets.xcassets/AppIcon.brandassets/App Icon.imagestack/Front.imagestacklayer/Content.imageset/head.png"
+    convert -interpolate Nearest -filter point -gravity center -extent 800X480 -background none -transparent white  "sm64ios/sm64tvos/Assets.xcassets/AppIcon.brandassets/App Icon.imagestack/Front.imagestacklayer/Content.imageset/head.png" "sm64ios/sm64tvos/Assets.xcassets/AppIcon.brandassets/App Icon.imagestack/Front.imagestacklayer/Content.imageset/front800x480.png"
     
     rm "sm64ios/sm64tvos/Assets.xcassets/AppIcon.brandassets/App Icon.imagestack/Front.imagestacklayer/Content.imageset/head.png"
 fi
